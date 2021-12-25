ab9077902a30a366082e1470bba018091b8f77d1
tidiview <francois.vidit@laposte.net>
2021-12-25 22:20:44 +0100

    fixed error on source 2240w

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -16,10 +16,10 @@
 			<picture>
 				<source srcset="
 				svelte-welcome-280.webp 280w, 
-				svelte-welcome-380.webp 380w, svelte-welcome-480.webp 480w, svelte-welcome-640.webp 640w, svelte-welcome-840.webp 840w, svelte-welcome-1280.webp 1280w, svelte-welcome-1600.webp 1600w, svelte-welcome-1920.webp 1920w, svelte-welcome-2240.webp 2240w" type="image/webp" />
+				svelte-welcome-380.webp 380w, svelte-welcome-480.webp 480w, svelte-welcome-640.webp 640w, svelte-welcome-840.webp 840w, svelte-welcome-1280.webp 1280w, svelte-welcome-1600.webp 1600w, svelte-welcome-1920.webp 1920w" type="image/webp" />
 				<source srcset="
 				svelte-welcome-280.png 280w, 
-				svelte-welcome-380.png 380w, svelte-welcome-480.png 480w, svelte-welcome-640.png 640w, svelte-welcome-840.png 840w, svelte-welcome-1280.png 1280w, svelte-welcome-1600.png 1600w, svelte-welcome-1920.png 1920w, svelte-welcome-2240.png 2240w" type="image/png" />
+				svelte-welcome-380.png 380w, svelte-welcome-480.png 480w, svelte-welcome-640.png 640w, svelte-welcome-840.png 840w, svelte-welcome-1280.png 1280w, svelte-welcome-1600.png 1600w, svelte-welcome-1920.png 1920w" type="image/png" />
 				<img src="svelte-welcome.png" alt="Welcome" width="2048" height="495" />
 			</picture>
 		</div>
