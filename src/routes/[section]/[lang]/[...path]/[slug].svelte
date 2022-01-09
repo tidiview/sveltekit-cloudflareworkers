e60a9601897fbc4ae4d3fdc5c891fa187c7a2de7
tidiview <francois.vidit@laposte.net>
2022-01-09 07:28:17 +0100

    restricted [...path] route to docs

--- src/routes/[section]/[lang]/[...path]/[slug].svelte
+++ src/routes/[section]/[lang]/[...path]/[slug].svelte
@@ -1,5 +1,5 @@
 <script context='module'>
-    const valid_sections = new Set(['blog', 'docs', 'profile']);
+    const valid_sections = new Set(['docs']);
     const valid_langs = new Set(['fr', 'ja', 'en']);
 	const valid_path = new Set(['paris', 'paris/louvre', 'paris/louvre/la-joconde', 'paris/vendome', 'versailles', 'versailles/palais', 'versailles/jardins', 'versailles/trianon', 'versailles/marly', 'reims', 'chateaux-de-la-loire', 'chateaux-de-la-loire/chenonceau', 'chateaux-de-la-loire/cheverny', 'mont-saint-michel', 'mont-saint-michel/arch-michel', 'bruges', 'bruges/hopital-saint-jean']);
 	const valid_slug = new Set(['louvre', 'la-joconde', 'vasari', 'gautier', 'vendome', 'place', 'palais', 'hercule', 'jardins', 'latone', 'phaeton', 'trianon', 'arachnee', 'tiresias', 'iris', 'flore', 'marly', 'pierre-denis-martin', 'remes', 'saint-remi', 'chenonceau', 'diane', 'cheverny', 'adonis', 'perseus', 'arch-michel', 'nom', 'apocalypse', 'autre-docs', 'legende-doree', 'hopital-saint-jean', 'saint-jean', 'sainte-ursule', 'martin']);
