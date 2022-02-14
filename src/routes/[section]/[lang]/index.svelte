18fcd43ac4524654273405927581965dbde9d242
tidiview <francois.vidit@laposte.net>
2022-02-15 00:22:05 +0100

    create programatic table of content for docs, blog sections fr, en, ja

--- src/routes/[section]/[lang]/index.svelte
+++ src/routes/[section]/[lang]/index.svelte
@@ -16,31 +16,47 @@
 		
 		// ${page.path}
 		// /docs/ja
-		const url = `${page.path}.json`;
+		const url = `/` + section + `/` + lang + `.json`;
 		const res = await fetch(url);
-		const data = await res.json();
-
-		if (res.ok) {
+		
+		if (!res.ok)
 			return {
-				props: {
-					data
-				}
+				status: res.status,
+				error: new Error(`Could not load ${url}`)
 			};
-		}
- 
+		
+		const data = await res.json();
+		
+		// docs/route-tree.docs.ja.json
+		const url2 = `/` + section + `/route-tree.` + section + `.` + lang  + `.json`;
+		const res2 = await fetch(url2);
+
+		if (!res2.ok)
+			return {
+				status: res2.status,
+				error: new Error(`Could not load ${url2}`)
+			}
+
+		const data2 = await res2.json();
+
 		return {
-			status: res.status,
-			error: new Error(`Could not load ${url}`)
+			props: {
+				data,
+				data2
+			}
 		};
 
 	}
 </script>
 
 <script>
+
     export let data;
     export let section = data.params.section;
     export let lang = data.params.lang;
-	console.log(data);
+	export let data2;
+		let indexData2 = data2.length;
+	console.log(data, data2, indexData2);
 </script>
 
 from [section] / [lang] / index.svelte,<br/>
@@ -51,3 +67,38 @@ this is: <br/>
 with:
 <pre>[section]: {section}</pre>
 <pre>[lang]: {lang}</pre>
+
+<ol>
+	{#each data2 as data2}
+	<li>
+		<a href="{data2['url']}" rel="bookmark">{data2['name']}</a>
+	</li>
+	{#if data2['children']}
+		<ul>
+			{#each data2['children'] as children}
+			<li>
+				<a href="{children['url']}" rel="bookmark">{children['name']}</a>
+			</li>
+			{#if children['children']}
+			<ul>
+				{#each children['children'] as children2}
+				<li>
+					<a href="{children2['url']}">{children2['name']}</a>
+				</li>
+				{#if children2['children']}
+					<ul>
+					{#each children2['children'] as children3}
+						<li>
+							<a href="{children3['url']}">{children3['name']}</a>
+						</li>
+					{/each}
+					</ul>
+				{/if}
+				{/each}
+			</ul>
+			{/if}
+			{/each}
+		</ul>
+	{/if}
+	{/each}
+</ol>
