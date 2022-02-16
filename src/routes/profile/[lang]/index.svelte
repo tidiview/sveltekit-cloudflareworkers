328f86e22ef497cae5092d0ae8b9f9ec88349f6e
tidiview <francois.vidit@laposte.net>
2022-02-16 15:37:03 +0100

    simplified logic of loading function, profile

--- src/routes/profile/[lang]/index.svelte
+++ src/routes/profile/[lang]/index.svelte
@@ -1,13 +1,12 @@
 <script context="module">
-    const valid_sections = new Set(['blog', 'docs', 'profile']);
     const valid_langs = new Set(['fr', 'ja', 'en']);
 
 	/** @type {import('@sveltejs/kit').Load} */
 	export async function load({ page, fetch }) {
         const section = 'profile';
         const lang = page.params.lang;
-        if (!valid_sections.has(section) || !valid_langs.has(lang)) {
-			console.log(`invalid parameter section: ${section} or lang: ${lang}`);
+        if (!valid_langs.has(lang)) {
+			console.log(`invalid parameter lang: ${lang}`);
 			return {
 				status: 404,
 				error: 'Not found'
@@ -15,7 +14,7 @@
 		}
 		
 		// ${page.path}
-		// /docs/ja
+		// /profile/ja
 		const url = `/profile/` + lang + `.json`;
 		const res = await fetch(url);
 		
@@ -57,7 +56,6 @@
     export let lang = data.params.lang;
 	export let data2;
 		let articleBody = data2.articleBody;
-	console.log(data2)
 </script>
 
 from [section] / [lang] / index.svelte,<br/>
